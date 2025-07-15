f081ec787d88a8ad756282e8cc29347ac0caa07d
jhughes <jhughe14@wvup.edu>
2025-07-15 15:22:16 -0400

    Unit converter project creation

---
+++ JHughes_UnitConverter.RazorPages/JHughes_UnitConverter.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JHughes_UnitConverter.RazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
