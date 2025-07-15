af9b2fdb01fbdba5b8efd0cc0af95c34b286b4bf
jhughes <jhughe14@wvup.edu>
2025-07-15 15:53:13 -0400

    test

--- JHughes_UnitConverter.RazorPages/JHughes_UnitConverter.RazorPages/obj/Debug/net9.0/JHughes_UnitConverter.RazorPages.AssemblyInfo.cs
+++ JHughes_UnitConverter.RazorPages/JHughes_UnitConverter.RazorPages/obj/Debug/net9.0/JHughes_UnitConverter.RazorPages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JHughes_UnitConverter.RazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f081ec787d88a8ad756282e8cc29347ac0caa07d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JHughes_UnitConverter.RazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JHughes_UnitConverter.RazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
